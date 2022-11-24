4ccb069f292ce71f8d5e71df09987eba06955f77
Riley Testut <riley@rileytestut.com>
2022-11-23 19:08:31 -0600

    Replaces StoreApp.setVersions() preconditionFailure with runtime error
    It’s more common than expected for apps to not have any app versions, so better to fail gracefully than crash.

--- AltStoreCore/Model/StoreApp.swift
+++ AltStoreCore/Model/StoreApp.swift
@@ -274,14 +274,12 @@ public class StoreApp: NSManagedObject, Decodable, Fetchable
             
             if let versions = try container.decodeIfPresent([AppVersion].self, forKey: .versions)
             {
-                //TODO: Throw error if there isn't at least one version.
-                
                 for version in versions
                 {
                     version.appBundleID = self.bundleIdentifier
                 }
                 
-                self.setVersions(versions)
+                try self.setVersions(versions)
             }
             else
             {
@@ -299,7 +297,7 @@ public class StoreApp: NSManagedObject, Decodable, Fetchable
                                                            size: Int64(size),
                                                            appBundleID: self.bundleIdentifier,
                                                            in: context)
-                self.setVersions([appVersion])
+                try self.setVersions([appVersion])
             }
         }
         catch
@@ -316,8 +314,12 @@ public class StoreApp: NSManagedObject, Decodable, Fetchable
 
 internal extension StoreApp
 {
-    func setVersions(_ versions: [AppVersion])
+    func setVersions(_ versions: [AppVersion]) throws
     {
+        guard let latestVersion = versions.first else {
+            throw MergeError.noVersions(for: self)
+        }
+        
         self._versions = NSOrderedSet(array: versions)
         
         let latestSupportedVersion = versions.first(where: { $0.isSupported })
@@ -337,7 +339,6 @@ internal extension StoreApp
         }
                 
         // Preserve backwards compatibility by assigning legacy property values.
-        guard let latestVersion = versions.first else { preconditionFailure("StoreApp must have at least one AppVersion.") }
         self.latestVersionString = latestVersion.version
         self._versionDate = latestVersion.date
         self._versionDescription = latestVersion.localizedDescription
@@ -375,7 +376,7 @@ public extension StoreApp
                                                    appBundleID: app.bundleIdentifier,
                                                    sourceID: Source.altStoreIdentifier,
                                                    in: context)
-        app.setVersions([appVersion])
+        try? app.setVersions([appVersion])
         
         print("makeAltStoreApp StoreApp: \(String(describing: app))")
         
