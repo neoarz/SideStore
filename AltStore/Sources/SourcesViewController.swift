689dc5954eb5aee5c9335556bce50c61a078fafc
Riley Testut <riley@rileytestut.com>
2023-10-17 15:55:51 -0500

    Fixes “Unable to satisfy constraints” warnings for SourcesViewController

--- AltStore/Sources/SourcesViewController.swift
+++ AltStore/Sources/SourcesViewController.swift
@@ -116,8 +116,6 @@ final class SourcesViewController: UICollectionViewController
             self.placeholderView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor),
             self.placeholderView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor),
             
-            self.placeholderView.leadingAnchor.constraint(equalTo: self.placeholderView.stackView.leadingAnchor),
-            self.placeholderView.trailingAnchor.constraint(equalTo: self.placeholderView.stackView.trailingAnchor),
             self.placeholderView.topAnchor.constraint(equalTo: self.placeholderView.stackView.topAnchor),
             self.placeholderView.bottomAnchor.constraint(equalTo: self.placeholderView.stackView.bottomAnchor),
         ])
