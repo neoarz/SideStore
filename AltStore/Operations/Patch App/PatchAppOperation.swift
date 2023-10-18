27ca2f285b53d31423331224be3819d660ab8b73
Riley Testut <riley@rileytestut.com>
2023-10-18 14:09:06 -0500

    Logs Fugu14-related events with OSLog

--- AltStore/Operations/Patch App/PatchAppOperation.swift
+++ AltStore/Operations/Patch App/PatchAppOperation.swift
@@ -197,7 +197,7 @@ private extension PatchAppOperation
                 }
             }
             
-            print("Downloaded OTA archive.")
+            Logger.fugu14.notice("Downloaded iOS OTA archive.")
             return archiveURL
             
             #endif
@@ -227,7 +227,7 @@ private extension PatchAppOperation
                 return .ok
             }
             
-            print("Extracted Spotlight from OTA archive.")
+            Logger.fugu14.notice("Extracted Spotlight from OTA archive.")
             return spotlightFileURL
             
             #endif
@@ -249,7 +249,7 @@ private extension PatchAppOperation
             let appBinaryURL = temporaryAppURL.appendingPathComponent(appName, isDirectory: false)
             try self.appPatcher.patchAppBinary(at: appBinaryURL, withBinaryAt: patchFileURL)
             
-            print("Patched \(app.name).")
+            Logger.fugu14.notice("Patched \(app.name, privacy: .public)!")
             return temporaryAppURL
         }
         .mapError { ($0 as NSError).withLocalizedFailure(String(format: NSLocalizedString("Could not patch %@ placeholder.", comment: ""), app.name)) }
