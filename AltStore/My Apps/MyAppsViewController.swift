7edb5716b7da07351a3d7ee6b77b73f3349f25c0
nythepegasus <nythepegasus84@gmail.com>
2024-05-06 20:29:55 -0400

    Fix typo

--- AltStore/My Apps/MyAppsViewController.swift
+++ AltStore/My Apps/MyAppsViewController.swift
@@ -1129,7 +1129,7 @@ private extension MyAppsViewController
             }
             catch
             {
-                print("Failed to activate app:", error)
+                print("Failed to deactivate app:", error)
                 
                 DispatchQueue.main.async {
                     installedApp.isActive = true
