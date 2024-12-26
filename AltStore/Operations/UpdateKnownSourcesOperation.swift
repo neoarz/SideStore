950e54a04bafb47647cfc6c6b478bb1031bb68b9
Magesh K <47920326+mahee96@users.noreply.github.com>
2024-12-26 22:00:38 +0530

    [trusted-apps]: update url to develop branch trustedapps.json (need to move it to proper location later)

--- AltStore/Operations/UpdateKnownSourcesOperation.swift
+++ AltStore/Operations/UpdateKnownSourcesOperation.swift
@@ -14,11 +14,9 @@ private extension URL
 {
     // TODO: @mahee96: update this to a non-branch specific (prod-ready) location like github.io repo similar to anisette servers URL list
     #if STAGING
-//    static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/develop/trustedapps.json")!
-    static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/rebase-2.0-wip/trustedapps.json")!
+   static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/develop/trustedapps.json")!
     #else
-//    static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/develop/trustedapps.json")!
-    static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/rebase-2.0-wip/trustedapps.json")!
+   static let sources = URL(string: "https://raw.githubusercontent.com/SideStore/SideStore/develop/trustedapps.json")!
     #endif
 }
 
